9f7559809152f0e42c3ded1c0e57c7802c771ef0
Adriano Costa <adriano.dmcosta@gmail.com>
2025-01-30 17:32:45 -0300

    Retirar codigo que apagas as imagens no ECR por falta de permissão para executa-lo. Será preciso apagar manualmente antes de executar o destroy

--- src/provider.tf
+++ src/provider.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
-}
-
 provider "aws" {
   region = var.regionDefault
 }
-
-provider "null" {}
--- src/scripts/clear_ecr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-REPOSITORY_NAME=$1
-
-aws ecr batch-delete-image --repository-name "$REPOSITORY_NAME" --image-ids "$(aws ecr list-images --repository-name "$REPOSITORY_NAME" --query 'imageIds[*].imageDigest' --output text)"
